0a0d78dc02fe0aef8c53fe439bb0ae9db28f39a4
hnsia <44498178+hnsia@users.noreply.github.com>
2023-11-28 01:08:09 +0800

    Add header component

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,26 @@
+<header
+	class="flex px-4 items-center h=[64px] py-2 bg-white z-10 justify-between sticky top-0 left-0"
+>
+	<div class="flex gap-4 items-center">
+		<img
+			class="p-3 rounded-full hover:bg-gray-200 cursor-pointer"
+			src="/icons/burgerIcon.svg"
+			alt=""
+		/>
+		<img src="/icons/ytLogo.svg" alt="" />
+	</div>
+	<div>Search Bar</div>
+	<div class="flex gap-3 items-center">
+		<img
+			src="/icons/createIcon.svg"
+			class="p-3 rounded-full hover:bg-gray-200 cursor-pointer"
+			alt=""
+		/>
+		<img
+			src="/icons/notificationIcon.svg"
+			class="p-3 rounded-full hover:bg-gray-200 cursor-pointer"
+			alt=""
+		/>
+		<img src="/avatar.png" class="rounded-full bg-gray-200 h-8 w-8 cursor-pointer" alt="" />
+	</div>
+</header>
