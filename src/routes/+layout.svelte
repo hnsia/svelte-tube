4832352baf430d8a2c3e76a9972f1bfaed43680c
hnsia <44498178+hnsia@users.noreply.github.com>
2023-11-30 00:02:01 +0800

    Add sidebar component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script>
 	import Header from '$lib/components/Header.svelte';
+	import Sidebar from '$lib/components/Sidebar.svelte';
 	import '../app.css';
 </script>
 
 <Header />
-<slot />
+<div class="flex">
+	<Sidebar />
+	<main class="ml-[74px] p-6">
+		<slot />
+	</main>
+</div>
